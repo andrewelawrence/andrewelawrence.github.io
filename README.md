104d2e96ef1a89cd093390f6598c66375a2b9d44
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2023-07-05 14:29:38 -0400

    Merge branch 'main' of https://github.com/andrewelawrence/andrewelawrence.github.io

--- README.md
+++
@@ -1,3 +0,0 @@
-Hi!
-Welcome to the github repo for my portfolio website. It is being actively developed so expect to see errors / disfunction.
-visit andrewelawrence.com for its active state. email me at andrew.e.lawrence@gmail.com for any questions or comments!
--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
+import "./Profile.css";
 
 export default function Profile() {
   return (
     <div className="profile-container">
       <div className="profile-parent">
         <div className="profile-details">
-          <div className="colz">
+          <div className="contact-icons">
             <a
               href="https://www.linkedin.com/in/andrew-e-lawrence/ "
               target="_blank"
@@ -21,12 +23,69 @@ export default function Profile() {
               <i className="fa fa-github-square"></i>
             </a>
             <a
+              href="https://www.thingiverse.com/andrewelawrence/designs"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa fa-crop"></i>
+            </a>
+            <a
               href="mailto:andrew.e.lawrence@gmail.com"
               target="_blank"
               rel="noopener noreferrer"
             >
               <i className="fa fa-inbox"></i>
             </a>
+          </div>
+          <div className="profile-name-and-typewriter">
+            <div className="profile-name">
+              <span>
+                {" "}
+                Hi, I'm{" "}
+                <span className="highlighted-text">Andrew Lawrence</span>
+              </span>
+            </div>
+            <div className="profile-typewriter">
+              <span>
+                {" "}
+                <h1>
+                  {" "}
+                  <Typewriter
+                    onInit={(typewriter) => {
+                      typewriter
+                        .changeDelay(60)
+                        .changeDeleteSpeed(20)
+                        .typeString("An")
+                        .typeString(
+                          " undergraduate student at Tufts University,"
+                        )
+                        .pauseFor(750)
+                        .deleteChars(44)
+                        .typeString(" developing programmer,")
+                        .pauseFor(750)
+                        .deleteChars(11)
+                        .typeString("CAD designer,")
+                        .pauseFor(750)
+                        .deleteChars(17)
+                        .typeString("er.")
+                        .start();
+                    }}
+                  />
+                </h1>
+                <span className="profile-tagline">
+                  POSSIBLY SOME TAGLINE GOES HERE.
+                </span>
+              </span>
+            </div>
+            <div className="resume-button">
+              <a
+                href="Andrew Lawrence Resume.pdf"
+                download="Andrew Lawrence Resume.pdf"
+              >
+                {" "}
+                <button className="get-resume"> Get My Resume </button>
+              </a>
+            </div>
           </div>
         </div>
       </div>
