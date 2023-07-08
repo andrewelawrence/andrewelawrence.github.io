7ef511009fae2693bbd842a506b9bf77afb9347d
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2023-07-08 17:51:16 -0400

    Formatting

--- client/src/pages/Nav-Bar.js
+++ client/src/pages/Nav-Bar.js
@@ -1,20 +1,28 @@
 import React from "react";
 import "./Nav-Bar.css";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-  return (  
+  return (
     <div className="NavBar">
-        <ul>
-            <li> <Link to="/">Home</Link></li>
-        </ul>
-        <ul>
-            <li> <Link to="/projects">Projects</Link></li>
-        </ul>
-        <ul>
-            <li> <Link to="/information">Information</Link></li>
-        </ul>
+      <ul>
+        <li>
+          {" "}
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          {" "}
+          <Link to="/projects">Projects</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          {" "}
+          <Link to="/information">Information</Link>
+        </li>
+      </ul>
     </div>
   );
 }
--- client/src/pages/Site-Buttons.js
+++ client/src/pages/Site-Buttons.js
@@ -4,45 +4,44 @@ import "./Site-Buttons.css";
 export default function SiteButtons() {
   return (
     <div className="contact-icons">
+      <a
+        href="https://www.linkedin.com/in/andrew-e-lawrence/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="linked-in-btn">
+          <i class="fa fa-linkedin"></i>
+        </button>
+      </a>
 
-            <a 
-              href="https://www.linkedin.com/in/andrew-e-lawrence/"
-              target="_blank"
-              rel="noopener noreferrer"
-            > 
-            <button className="linked-in-btn">
-              <i class="fa fa-linkedin"></i>
-            </button>
-            </a>
+      <a
+        href="https://github.com/andrewelawrence"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="github-btn">
+          <i class="fa fa-github-square"></i>
+        </button>
+      </a>
 
-            <a 
-              href="https://github.com/andrewelawrence"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            <button className="github-btn">
-              <i class="fa fa-github-square"></i>
-            </button>
-            </a>
-
-            <a 
-              href="https://www.thingiverse.com/andrewelawrence/designs"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            <button className="thingiverse-btn">
-              <i class="fa fa-crop"></i>
-            </button>
-            </a>
-            <a 
-              href="mailto:andrew.e.lawrence@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            <button className="inbox-btn">
-              <i class="fa fa-inbox"></i>
-            </button>
-            </a>
-          </div>
+      <a
+        href="https://www.thingiverse.com/andrewelawrence/designs"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="thingiverse-btn">
+          <i class="fa fa-crop"></i>
+        </button>
+      </a>
+      <a
+        href="mailto:andrew.e.lawrence@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="inbox-btn">
+          <i class="fa fa-inbox"></i>
+        </button>
+      </a>
+    </div>
   );
-  }
+}
