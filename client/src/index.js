5dbceee0503efb3e16a23e85d5f2f39f808fad74
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2023-07-08 17:47:04 -0400

    Formatting

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Information from "./pages/Information";
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,59 +9,54 @@ export default function Home() {
   return (
     <>
       <SiteButtons />
-          <div className="profile-name-and-typewriter">
-            <div className="profile-name">
-              <span>
-                {" "}
-                Hi, I'm{" "}
-                <span className="highlighted-text">Andrew Lawrence</span>
-              </span>
-            </div>
-            <div className="profile-typewriter">
-              <span>
-                {" "}
-                <h1>
-                  {" "}
-                  <Typewriter
-                    onInit={(typewriter) => {
-                      typewriter
-                        .changeDelay(60)
-                        .changeDeleteSpeed(20)
-                        .typeString("An")
-                        .typeString(
-                          " undergraduate student at Tufts University,"
-                        )
-                        .pauseFor(750)
-                        .deleteChars(44)
-                        .typeString(" developing programmer,")
-                        .pauseFor(750)
-                        .deleteChars(11)
-                        .typeString("CAD designer,")
-                        .pauseFor(750)
-                        .deleteChars(17)
-                        .typeString("er.")
-                        .start();
-                    }}
-                  />
-                </h1>
-                {/* <span className="profile-tagline">
+      <div className="profile-name-and-typewriter">
+        <div className="profile-name">
+          <span>
+            {" "}
+            Hi, I'm <span className="highlighted-text">Andrew Lawrence</span>
+          </span>
+        </div>
+        <div className="profile-typewriter">
+          <span>
+            {" "}
+            <h1>
+              {" "}
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .changeDelay(60)
+                    .changeDeleteSpeed(20)
+                    .typeString("An")
+                    .typeString(" undergraduate student at Tufts University,")
+                    .pauseFor(750)
+                    .deleteChars(44)
+                    .typeString(" developing programmer,")
+                    .pauseFor(750)
+                    .deleteChars(11)
+                    .typeString("CAD designer,")
+                    .pauseFor(750)
+                    .deleteChars(17)
+                    .typeString("er.")
+                    .start();
+                }}
+              />
+            </h1>
+            {/* <span className="profile-tagline">
                 POSSIBLY SOME TAGLINE GOES HERE.
                 </span> */}
-              </span>
-            </div>
-            <div className="resume-button">
-              <a
-                href="Andrew Lawrence Resume.pdf"
-                download="Andrew Lawrence Resume.pdf"
-              >
-                {" "}
-                <button className="get-resume"> Get My Resume </button>
-              </a>
-            </div>
-          </div>
+          </span>
+        </div>
+        <div className="resume-button">
+          <a
+            href="Andrew Lawrence Resume.pdf"
+            download="Andrew Lawrence Resume.pdf"
+          >
+            {" "}
+            <button className="get-resume"> Get My Resume </button>
+          </a>
+        </div>
+      </div>
       <NavBar />
-      </>
-
-    
+    </>
   );
 }
