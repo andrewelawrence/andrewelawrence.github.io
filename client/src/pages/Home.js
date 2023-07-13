5e8feaf9c3535db69e05b8cd831ade1c7a3c4268
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2023-07-13 18:34:13 -0400

    7/13 more edits

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
 import "./Home.css";
-import "../App.css";
 import NavBar from "./Nav-Bar";
 import SiteButtons from "./Site-Buttons";
 
@@ -9,53 +8,43 @@ export default function Home() {
   return (
     <>
       <SiteButtons />
-      <div className="profile-name-and-typewriter">
-        <div className="profile-name">
-          <span>
-            {" "}
-            Hi, I'm <span className="highlighted-text">Andrew Lawrence</span>
-          </span>
-        </div>
-        <div className="profile-typewriter">
-          <span>
-            {" "}
-            <h1>
-              {" "}
-              <Typewriter
-                onInit={(typewriter) => {
-                  typewriter
-                    .changeDelay(60)
-                    .changeDeleteSpeed(20)
-                    .typeString("An")
-                    .typeString(" undergraduate student at Tufts University,")
-                    .pauseFor(750)
-                    .deleteChars(44)
-                    .typeString(" developing programmer,")
-                    .pauseFor(750)
-                    .deleteChars(11)
-                    .typeString("CAD designer,")
-                    .pauseFor(750)
-                    .deleteChars(17)
-                    .typeString("er.")
-                    .start();
-                }}
-              />
-            </h1>
-            {/* <span className="profile-tagline">
-                POSSIBLY SOME TAGLINE GOES HERE.
-                </span> */}
-          </span>
-        </div>
+      <div className="profile-name">
+        <span> Hi, I'm Andrew Lawrence</span>
       </div>
-      <div className="resume-button">
-        <a
-          href="Andrew Lawrence Resume.pdf"
-          download="Andrew Lawrence Resume.pdf"
-        >
+      <div className="Typewriter">
+        <span>
           {" "}
-          <button className="get-resume"> Get My Resume </button>
-        </a>
+          <h1>
+            {" "}
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .changeDelay(60)
+                  .changeDeleteSpeed(20)
+                  .typeString("An")
+                  .typeString(" undergraduate student at Tufts University,")
+                  .pauseFor(750)
+                  .deleteChars(44)
+                  .typeString(" developing programmer,")
+                  .pauseFor(750)
+                  .deleteChars(11)
+                  .typeString("CAD designer,")
+                  .pauseFor(750)
+                  .deleteChars(17)
+                  .typeString("er.")
+                  .start();
+              }}
+            />
+          </h1>
+        </span>
       </div>
+      {/* <div className="Bio">
+          <h1></h1>
+          <span className="profile-tagline">
+            I firmly believe that in our to safe guard our future we must focus
+            on safe and smart product design.
+          </span>
+        </div> */}
       <NavBar />
     </>
   );
--- client/src/pages/Nav-Bar.js
+++ client/src/pages/Nav-Bar.js
@@ -4,25 +4,40 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div className="NavBar">
-      <ul className="Home">
-        <li>
+    <>
+      <div className="NavBar">
+        <ul className="Home">
+          <li>
+            {" "}
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="Projects">
+          <li>
+            {" "}
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="Information">
+          <li>
+            {" "}
+            <Link to="/information">Information</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="Get-Resume">
+        <a
+          href="Andrew Lawrence Resume.pdf"
+          download="Andrew Lawrence Resume.pdf"
+        >
           {" "}
-          <Link to="/">Home</Link>
-        </li>
-      </ul>
-      <ul className="Projects">
-        <li>
-          {" "}
-          <Link to="/projects">Projects</Link>
-        </li>
-      </ul>
-      <ul className="Information">
-        <li>
-          {" "}
-          <Link to="/information">Information</Link>
-        </li>
-      </ul>
-    </div>
+          <button className="Resume"> Want my resume? </button>
+        </a>
+      </div>
+    </>
   );
 }
