777ae825585ef8a2121d4f034c7fd30d50ace087
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2023-08-04 17:04:12 -0400

    Project page resolution 8/4

--- client/src/pages/Information.js
+++ client/src/pages/Information.js
@@ -7,7 +7,35 @@ export default function Information() {
     <>
       <SiteButtons />
       <div className="information-page">
-        <h1> INFORMATION PAGE </h1>
+        <div className="aboutMe">
+          <h1> About Me. </h1>
+          <ul>Home : Lexington MA</ul>
+          <ul>School : Tufts 2026</ul>
+          <ul></ul>
+          <p>
+            I'm an undergraduate student who believes that tech product design
+          </p>
+        </div>
+        <div className="interests">
+          <h1>My Interests.</h1>
+          <p>3D printing <span>Watching that first layer be put down never gets old! I love </span></p>
+          <p>PC hardware and design</p>
+          <p>
+            Spending time outside{" "}
+            <span>
+              I'm an avid hiker, biker, and general -iker. I love playing soccer
+              in the summer and skiing in the winter.
+            </span>
+          </p>
+          <p>
+            Sci-Fi Novels{" "}
+            <span>
+              I'm currently reading <i>Time Enough For Love</i> by Robert A.
+              Heinlein. If you have any recommendations, feel free to reach out
+              to me!{" "}
+            </span>
+          </p>
+        </div>
       </div>
     </>
   );
--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -7,7 +7,32 @@ export default function Projects() {
     <>
       <SiteButtons />
       <div className="projects-page">
-        <h1> PROJECTS PAGE </h1>
+        <h1 className="AIessay">Safe AI Development Research Paper</h1>
+        <p>
+          <a
+            href="ML-growth-paper.pdf"
+            className="AIessayTitle"
+            download="ML-growth-paper.pdf"
+          >
+            | "Understanding the Risks Posed with the Unregulated ML System
+            Growth" |
+          </a>
+        </p>
+        <p>
+          This paper dives deep into the risks that unaligned AI pose to our
+          society, as well as challenges faced by developers in alignment.
+          Besides highlighting those issues, it also bolstered my interest in
+          understanding how product development shapes our world.{" "}
+        </p>
+        <br></br>
+        <h1 className="SpotifyPlugin">Smart Spotify Lyrics Display</h1>
+        <p>
+          I am currently developing a windows app to display real-time Spotify
+          song lyrics using Musixmatch's API service on any Windows screen
+          (GitHub link soon). This was based out of frustration with Spotify's
+          windows integration, as users are required to keep Spotify open in
+          order to view the lyrics to their songs.
+        </p>
       </div>
     </>
   );
