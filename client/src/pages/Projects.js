0dddd5ac0d9f0a9277fde3365dc2119cb293449a
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2023-09-28 21:44:03 -0400

    Project Page update

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -7,31 +7,30 @@ export default function Projects() {
     <>
       <SiteButtons />
       <div className="projects-page">
-        <h1 className="AIessay">Safe AI Development Research Paper</h1>
+        <h1 className="AIessay"> Risk Analysis of Growth in AI Capabilities</h1>
         <p>
           <a
             href="ML-growth-paper.pdf"
             className="AIessayTitle"
             download="ML-growth-paper.pdf"
           >
-            | "Understanding the Risks Posed with the Unregulated ML System
-            Growth" |
+            | "Understanding the Risks Posed with Unregulated ML System Growth"
+            |
           </a>
         </p>
         <p>
           This paper dives deep into the risks that unaligned AI pose to our
-          society, as well as challenges faced by developers in alignment.
-          Besides highlighting those issues, it also bolstered my interest in
-          understanding how product development shapes our world.{" "}
+          society, as well as the technical barriers faced by developers in
+          alignment. Besides highlighting those issues, it also bolstered my
+          interest in understanding how product development shapes our world.{" "}
         </p>
         <br></br>
-        <h1 className="SpotifyPlugin">Smart Spotify Lyrics Display</h1>
+        <h1 className="SpotifyPlugin">3D Printer Firmware Development</h1>
         <p>
-          I am currently developing a windows app to display real-time Spotify
-          song lyrics using Musixmatch's API service on any Windows screen
-          (GitHub link soon). This was based out of frustration with Spotify's
-          windows integration, as users are required to keep Spotify open in
-          order to view the lyrics to their songs.
+          Besides the pratical fabrication of parts to increase printer
+          efficency, I also researched and developed a custom Marlin firmware
+          for the Ender 3 Pro 4.2.2 printer with a BL-Touch auto leveling system
+          and custom power supply.
         </p>
       </div>
     </>
