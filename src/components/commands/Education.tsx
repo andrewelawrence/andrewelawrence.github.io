7b107dd90738f7e62fdd66cc11cd1b289b3a4e36
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2024-11-24 20:08:10 -0300

    Update `education` command

--- src/components/commands/Education.tsx
+++ src/components/commands/Education.tsx
@@ -1,28 +1,29 @@
-import { EduIntro, EduList } from "../styles/Education.styled";
-import { Wrapper } from "../styles/Output.styled";
+import { EduIntro, EduList, EndEduList } from '../styles/Education.styled';
+import { Wrapper } from '../styles/Output.styled';
 
 const Education: React.FC = () => {
-  return (
-    <Wrapper data-testid="education">
-      {eduBg.map(({ title, desc }) => (
-        <EduList key={title}>
-          <div className="title">{title}</div>
-          <div className="desc">{desc}</div>
-        </EduList>
-      ))}
-    </Wrapper>
-  );
+        return (
+                <Wrapper data-testid="education">
+                        {eduBg.map(({ title, desc }) => (
+                                <EduList key={title}>
+                                        <div className="title">{title}</div>
+                                        <div className="desc">{desc}</div>
+                                </EduList>
+                        ))}
+                        <EndEduList />
+                </Wrapper>
+        );
 };
 
 const eduBg = [
-  {
-    title: "B.Sc in Computer Science",
-    desc: "Tufts University | 2022 - 2026 (In Progress)",
-  },
-  {
-    title: "B.A in Science, Technology, and Society",
-    desc: "Tufts University | 2022 - 2026 (In Progress)",
-  }
+        {
+                title: 'B.Sc in Computer Science',
+                desc: 'Tufts University | 2022 - 2026 (In Progress)',
+        },
+        {
+                title: 'B.A in Science, Technology, and Society',
+                desc: 'Tufts University | 2022 - 2026 (In Progress)',
+        },
 ];
 
 export default Education;
--- src/components/styles/Education.styled.tsx
+++ src/components/styles/Education.styled.tsx
@@ -1,20 +1,18 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const EduIntro = styled.div`
-
-`;
+export const EduIntro = styled.div``;
 
 export const EduList = styled.div`
-  margin-bottom: 1rem;
-  .title {
-    font-weight: 700;
-  }
+        margin-bottom: 1rem;
+        .title {
+                font-weight: 700;
+        }
 
-  .desc {
-    color: ${({ theme }) => theme.colors?.text.light};
-  }
+        .desc {
+                color: ${({ theme }) => theme.colors?.text.light};
+        }
 `;
 
 export const EndEduList = styled.div`
-  margin-bottom: -1rem;
+        margin-bottom: -1rem;
 `;
