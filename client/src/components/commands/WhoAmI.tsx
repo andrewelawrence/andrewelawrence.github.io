1c29f60ed6757d4c68ca74ffda80cdc2af372012
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2024-11-24 20:34:48 -0300

    Update `whoami`

--- client/src/components/commands/WhoAmI.tsx
+++ client/src/components/commands/WhoAmI.tsx
@@ -3,7 +3,7 @@
 const WhoAmI: React.FC = () => {
         return (
                 <div>
-                        I am not too sure. Maybe come back in awhile and I'll
+                        I am not too sure. Maybe come back and I'll
                         know.
                 </div>
         );
