d62ca3c6ce4b2b477623e49d135e4b8007a0e714
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2024-11-24 20:05:29 -0300

    Update `about` command

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -1,26 +1,37 @@
 import {
-  AboutWrapper,
-  HighlightAlt,
-  HighlightSpan,
-} from "../styles/About.styled";
+        AboutWrapper,
+        HighlightAlt,
+        HighlightSpan,
+} from '../styles/About.styled';
 
 const About: React.FC = () => {
-  return (
-    <AboutWrapper data-testid="about">
-      <p>
-        Hi, I'm <HighlightSpan>Andrew Lawrence</HighlightSpan>!
-        <br></br>
-        <br></br>
-        I'm a undergraduate student at Tufts University (Class of 2026).
-        <br></br>
-        <br></br>
-        I'm passionate about <HighlightSpan>AI Governance</HighlightSpan> research and policy, especially <br></br> algorithmic fairness (AF) and human-algorithm interaction (HAI).
-        <br></br>
-        <br></br>
-        When I'm not looking up how to code, I enjoy reading & writing, <br></br> bouldering, and cribbage, among many other things.
-      </p>
-    </AboutWrapper>
-  );
+        return (
+                <AboutWrapper data-testid="about">
+                        <p>
+                                Hi, I'm{' '}
+                                <HighlightSpan>Andrew Lawrence</HighlightSpan>!
+                                <br />
+                                <br />
+                                I'm a undergraduate student at Tufts University
+                                (Class of 2026).
+                                <br />
+                                <br />
+                                I'm passionate about{' '}
+                                <HighlightSpan>
+                                        AI Governance
+                                </HighlightSpan>{' '}
+                                research and policy, especially
+                                <br /> algorithmic fairness (AF) and
+                                human-algorithm interaction (HAI).
+                                <br />
+                                <br />
+                                When I'm not looking up how the LEA instruction
+                                works, I enjoy reading & writing,
+                                <br /> bouldering, and cribbage, among many
+                                other things.
+                        </p>
+                </AboutWrapper>
+        );
 };
 
 export default About;
