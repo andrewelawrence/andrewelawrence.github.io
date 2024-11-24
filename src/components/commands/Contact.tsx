6f95facdc7dcb2a5ffec3001fdffc98dd872c49e
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2024-11-24 20:07:59 -0300

    Update `contact` command

--- src/components/commands/Contact.tsx
+++ src/components/commands/Contact.tsx
@@ -1,70 +1,66 @@
-import { useContext, useEffect } from "react";
-import { ProjectsIntro } from "../styles/Projects.styled";
-import { Cmd, CmdDesc, CmdList, HelpWrapper } from "../styles/Help.styled";
+import { useContext, useEffect } from 'react';
+import { ContactsContainer, ContactsIntro, ContactListEnd } from '../styles/Contact.styled';
+import { CmdDesc, CmdList, HelpWrapper } from '../styles/Help.styled';
 import {
-  checkRedirect,
-  generateTabs,
-  getCurrentCmdArry,
-  isArgInvalid,
-} from "../../utils/funcs";
-import { termContext } from "../Terminal";
-import Usage from "../Usage";
+        checkRedirect,
+        generateTabs,
+        getCurrentCmdArry,
+        isArgInvalid,
+} from '../../utils/funcs';
+import { termContext } from '../Terminal';
+import Usage from '../Usage';
+import { Cmd, Link } from '../styles/Output.styled';
 
 const Contacts: React.FC = () => {
-  const { arg, history, rerender } = useContext(termContext);
-
-  /* ===== get current command ===== */
-  const currentCommand = getCurrentCmdArry(history);
-
-  /* ===== check current command makes redirect ===== */
-  useEffect(() => {
-    if (checkRedirect(rerender, currentCommand, "contact")) {
-      contacts.forEach(({ id, url }) => {
-        id === parseInt(arg[1]) && window.open(url, "_blank");
-      });
-    }
-  }, [arg, rerender, currentCommand]);
-
-  /* ===== check arg is valid ===== */
-  const checkArg = () =>
-    isArgInvalid(arg, "go", ["1", "2", "3", "4"]) ? (
-      <Usage cmd="contact" />
-    ) : null;
-
-  return arg.length > 0 || arg.length > 2 ? (
-    checkArg()
-  ) : (
-    <HelpWrapper data-testid="contact">
-      {/* <ProjectsIntro>Here are my social links</ProjectsIntro> */}
-      {contacts.map(({ id, title, url, tab }) => (
-        <CmdList key={title}>
-          <Cmd>{`${id}. ${title}`}</Cmd>
-          {generateTabs(tab)}
-          <CmdDesc> {url}</CmdDesc>
-        </CmdList>
-      ))}
-      <Usage cmd="contact" marginY />
-    </HelpWrapper>
-  );
+        return (
+                <HelpWrapper data-testid="contact">
+                        <ContactsIntro></ContactsIntro>
+                        <ContactsContainer>
+                                {contacts.map(({ title, url, tabs }) => (
+                                        <CmdList key={title}>
+                                                <Cmd>{`${title}`}</Cmd>
+                                                {generateTabs(tabs)}--&nbsp;
+                                                <Link
+                                                        href={url}
+                                                        target="_blank"
+                                                >
+                                                        {url}
+                                                </Link>
+                                        </CmdList>
+                                ))}
+                                <ContactListEnd /> <br />
+                        I go by @andrewelawrence or @exton on most platforms.
+                        </ContactsContainer>
+                </HelpWrapper>
+        );
 };
 
 const contacts = [
-  {
-    id: 1,
-    title: "Email",
-    url: "mailto:andrew.e.lawrence@gmail.com",
-    tab: 4,
-  },
-  {
-    id: 2,
-    title: "GitHub",
-    url: "https://github.com/andrewelawrence",
-    tab: 3,
-  },
-  {
-    id: 3,
-    title: "LinkedIn",
-    url: "https://www.linkedin.com/in/andrew-e-lawrence/",
-    tab: 1,
-  },
+        {
+                title: 'Email',
+                url: 'https://mailto:andrew.e.lawrence@gmail.com',
+                tabs: 6,
+        },
+        {
+                title: 'LinkedIn',
+                url: 'https://www.linkedin.com/in/andrew-e-lawrence/',
+                tabs: 3,
+        },
+        {
+                title: 'GitHub',
+                url: 'https://github.com/andrewelawrence',
+                tabs: 5,
+        },
+        // {
+        //         title: 'Instagram',
+        //         url: 'https://www.instagram.com/andrewelawrence/',
+        //         tabs: 2,
+        // },
+        // {
+        //         title: 'Mastodon',
+        //         url: 'https://mastodon.social/@exton',
+        //         tabs: 3,
+        // },
 ];
+
+export default Contacts;
--- src/components/styles/Contact.styled.tsx
+++ src/components/styles/Contact.styled.tsx
@@ -1,24 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const ProjectContainer = styled.div`
-  margin-top: 0.5rem;
-  margin-bottom: 0.875rem;
+export const ContactsContainer = styled.div`
 `;
 
-export const ProjectsIntro = styled.div`
-  margin-top: 0.5rem;
-  margin-bottom: 1rem;
-//   line-height: 1.5rem;
+export const ContactsIntro = styled.div`
+        max-width: 500px;
 `;
 
-export const ProjectTitle = styled.div`
-  font-weight: 700;
-//   margin-bottom: 0.25rem;
-`;
-
-export const ProjectDesc = styled.div`
-  color: ${({ theme }) => theme.colors?.text.light};
-//   text-align: justify;
-//   line-height: 1.5rem;
-  max-width: 500px;
+export const ContactListEnd = styled.div`
 `;
