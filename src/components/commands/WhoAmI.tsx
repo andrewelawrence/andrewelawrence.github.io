214a9364851970bf50b3b2b96ae11145b563c130
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2024-11-24 20:07:05 -0300

    Restore `whoami` command

---
+++ src/components/commands/WhoAmI.tsx
@@ -0,0 +1,7 @@
+// import { } from "../styles/WhoAmI.styled";
+
+const WhoAmI: React.FC = () => {
+        return <div>I am not too sure. Maybe come back in awhile and I'll know.</div>;
+};
+
+export default WhoAmI;
