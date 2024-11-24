33c3f2bbe4813226223381f2e8e2b9792cd29489
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2024-11-24 20:05:43 -0300

    Update `banner` command

--- src/components/commands/Banner.tsx
+++ src/components/commands/Banner.tsx
@@ -1,18 +1,17 @@
 import {
-  Cmd,
-  HeroContainer,
-  Link,
-  PreName,
-  PreNameMobile,
-  PreWrapper,
-} from "../styles/Banner.styled";
+        HeroContainer,
+        PreName,
+        PreNameMobile,
+        PreWrapper,
+} from '../styles/Banner.styled';
+import { Cmd, Link } from '../styles/Output.styled';
 
 const Banner: React.FC = () => {
-  return (
-    <HeroContainer data-testid="banner">
-      <div className="info-section">
-        <PreName>
-          {`        
+        return (
+                <HeroContainer data-testid="banner">
+                        <div className="info-section">
+                                <PreName>
+                                        {`        
                    888
                    888
                    888
@@ -22,10 +21,10 @@ const Banner: React.FC = () => {
  Y8b.     .d8""8b. Y88b.  Y88..88P 888  888
  "Y8888  888  888  "Y888  "Y88P"  888  888"
           `}
-        </PreName>
-        <PreWrapper>
-          <PreNameMobile>
-            {`
+                                </PreName>
+                                <PreWrapper>
+                                        <PreNameMobile>
+                                                {`
                    888
                    888
                    888
@@ -35,22 +34,27 @@ const Banner: React.FC = () => {
  Y8b.     .d8""8b. Y88b.  Y88..88P 888  888
  "Y8888  888  888  "Y888  "Y88P"  888  888"
           `}
-          </PreNameMobile>
-        </PreWrapper>
-        {/* <div>What did you think the 'e' stood for?</div> */}
-        <div>
-          This project's source code can be found in it's{" "}
-          <Link href="https://github.com/andrewelawrence/andrewelawrence.github.io">
-            GitHub repo
-          </Link>
-          .
-        </div>
-        <div>
-          Type `<Cmd>help</Cmd>` for a list of available commands.
-        </div>
-      </div>
-    </HeroContainer>
-  );
+                                        </PreNameMobile>
+                                </PreWrapper>
+                                {/* <div>What did you think the 'e' stood for?</div> */}
+                                <div>
+                                        This project's source code can be found
+                                        in it's{' '}
+                                        <Link
+                                                href="https://github.com/andrewelawrence/andrewelawrence.github.io"
+                                                target="_blank"
+                                        >
+                                                GitHub repo
+                                        </Link>
+                                        .
+                                </div>
+                                <div>
+                                        Type `<Cmd>help</Cmd>` for a list of
+                                        available commands.
+                                </div>
+                        </div>
+                </HeroContainer>
+        );
 };
 
 export default Banner;
--- src/components/styles/Banner.styled.tsx
+++ src/components/styles/Banner.styled.tsx
@@ -1,65 +1,49 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const HeroContainer = styled.div`
-//   display: flex;
-//   flex-wrap: wrap-reverse;
+        //   display: flex;
+        //   flex-wrap: wrap-reverse;
 
-//   @media (max-width: 932px) {
-//     margin-bottom: 1.5rem;
-//   }
+        //   @media (max-width: 932px) {
+        //     margin-bottom: 1.5rem;
+        //   }
 
-  div {
-    @media (min-width: 1024px) {
-      flex-basis: 50%;
-    }
-  }
+        div {
+                @media (min-width: 1024px) {
+                        flex-basis: 50%;
+                }
+        }
 `;
 
 export const PreName = styled.pre`
-//   margin-top: 0.5rem;
-//   margin-bottom: 1.5rem;
+        //   margin-top: 0.5rem;
+        //   margin-bottom: 1.5rem;
 
-  @media (max-width: 550px) {
-    display: none;
-  }
+        @media (max-width: 550px) {
+                display: none;
+        }
 `;
 
 export const PreWrapper = styled.div`
-  text-align: center;
+        text-align: center;
 `;
 
 export const PreNameMobile = styled.pre`
-//   margin-top: 0.5rem;
-//   margin-bottom: 1.5rem;
+        //   margin-top: 0.5rem;
+        //   margin-bottom: 1.5rem;
 
-  @media (min-width: 550px) {
-    display: none;
-  }
+        @media (min-width: 550px) {
+                display: none;
+        }
 `;
 
 export const PreImg = styled.pre`
-  @media (max-width: 550px) {
-    display: none;
-  }
+        @media (max-width: 550px) {
+                display: none;
+        }
 `;
 
 export const Seperator = styled.div`
-//   margin-top: 0.75rem;
-//   margin-bottom: 0.75rem;
-`;
-
-export const Cmd = styled.span`
-  color: ${({ theme }) => theme.colors?.text.commands};
-`;
-
-export const Link = styled.a`
-  color: ${({ theme }) => theme.colors?.text.links};
-  text-decoration: none;
-//   line-height: 1.5rem;
-  white-space: nowrap;
-  border-bottom: 1px dashed ${({ theme }) => theme.colors?.text.links};
-
-  &:hover {
-    border-bottom-style: solid;
-  }
+        //   margin-top: 0.75rem;
+        //   margin-bottom: 0.75rem;
 `;
