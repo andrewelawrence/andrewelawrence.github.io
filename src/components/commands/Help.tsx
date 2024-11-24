7a07c28c1298a39cbde425d13fc311b5a7a2be7a
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2024-11-24 20:09:58 -0300

    Update `help` command

--- src/components/commands/Help.tsx
+++ src/components/commands/Help.tsx
@@ -1,30 +1,41 @@
 import {
-  Cmd,
-  CmdDesc,
-  CmdList,
-  HelpWrapper,
-  KeyContainer,
-} from "../styles/Help.styled";
-import { commands } from "../Terminal";
-import { generateTabs } from "../../utils/funcs";
+        CmdDesc,
+        CmdList,
+        HelpWrapper,
+        KeyContainer,
+} from '../styles/Help.styled';
+
+import { Cmd } from '../styles/Output.styled';
+import { commands } from '../Terminal';
+import { generateTabs } from '../../utils/funcs';
 
 const Help: React.FC = () => {
-  return (
-    <HelpWrapper data-testid="help">
-      {commands.map(({ cmd, desc, tab }) => (
-        <CmdList key={cmd}>
-          <Cmd>{cmd}</Cmd>
-          {generateTabs(tab)}
-          <CmdDesc>- {desc}</CmdDesc>
-        </CmdList>
-      ))}
-      <KeyContainer>
-        <div>Tab or Ctrl + i&nbsp; =&gt; autocompletes the command</div>
-        <div>Up Arrow {generateTabs(5)} =&gt; go back to previous command</div>
-        <div>Ctrl + l {generateTabs(5)} =&gt; clear the terminal</div>
-      </KeyContainer>
-    </HelpWrapper>
-  );
+        return (
+                <HelpWrapper data-testid="help">
+                        {commands.map(({ cmd, desc, tab }) => (
+                                <CmdList key={cmd}>
+                                        <Cmd>{cmd}</Cmd>
+                                        {generateTabs(tab)}
+                                        <CmdDesc>-- {desc}</CmdDesc>
+                                </CmdList>
+                        ))}
+                        <br />
+                        <KeyContainer>
+                                <div>
+                                        Tab or Ctrl + I&nbsp;=&gt; autocompletes
+                                        the command
+                                </div>
+                                <div>
+                                        Up Arrow {generateTabs(4)} =&gt; go back
+                                        to previous command
+                                </div>
+                                <div>
+                                        Ctrl + L {generateTabs(4)} =&gt; clear
+                                        the terminal and history
+                                </div>
+                        </KeyContainer>
+                </HelpWrapper>
+        );
 };
 
 export default Help;
--- src/components/styles/Help.styled.tsx
+++ src/components/styles/Help.styled.tsx
@@ -1,36 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const HelpWrapper = styled.div`
-//   margin-top: 0.25rem;
-//   margin-bottom: 0.75rem;
-`;
+export const HelpWrapper = styled.div``;
 
-export const CmdList = styled.div`
-//   margin-bottom: 0.25rem;
-`;
-
-export const Cmd = styled.span`
-  color: ${({ theme }) => theme.colors?.text.commands};
-`;
+export const CmdList = styled.div``;
 
 export const CmdDesc = styled.span`
-  color: ${({ theme }) => theme.colors?.text.plain};
-//   margin-bottom: 0.75rem;
+        color: ${({ theme }) => theme.colors?.text.plain};
 
-  @media (max-width: 550px) {
-    display: block;
-  }
+        @media (max-width: 550px) {
+                display: block;
+        }
 `;
 
 export const KeyContainer = styled.div`
-//   font-size: 0.875rem;
-//   margin-top: 1rem;
-
-  @media (max-width: 550px) {
-    display: none;
-  }
-
-//   div {
-//     margin-top: 0.25rem;
-//   }
+        @media (max-width: 550px) {
+                display: none;
+        }
 `;
