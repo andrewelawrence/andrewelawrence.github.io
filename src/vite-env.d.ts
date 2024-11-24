4844878d9302e2f8ea978e1bdfce0e97cd982174
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2024-11-24 20:09:18 -0300

    Update theme customization

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,40 +1,28 @@
 /// <reference types="vite/client" />
 
-import "styled-components";
+import 'styled-components';
 
-declare module "styled-components" {
-  export interface DefaultTheme {
-    id: string;
-    name: string;
-    colors: {
-      background: string;
-      cursor: string;
-      scrollTrack: string;
-      scrollHandle: string;
-      scrollHandleHover: string;
-      text: {
-        plain: string;
-        light: string;
-        dark: string;
-        bold: string;
-        primary: string;
-        secondary: string;
-        tertiary: string;
-        commands: tring;
-        links: string;
-      };
-    };
-  }
+declare module 'styled-components' {
+        export interface DefaultTheme {
+                id: string;
+                name: string;
+                colors: {
+                        background: string;
+                        cursor: string;
+                        scrollTrack: string;
+                        scrollHandle: string;
+                        scrollHandleHover: string;
+                        text: {
+                                plain: string;
+                                light: string;
+                                dark: string;
+                                bold: string;
+                                primary: string;
+                                secondary: string;
+                                tertiary: string;
+                                commands: tring;
+                                links: string;
+                        };
+                };
+        }
 }
-
-//       body: string;
-//       scrollHandle: string;
-//       scrollHandleHover: string;
-//       primary: string;
-//       secondary: string;
-//       text: {
-//         100: string;
-//         200: string;
-//         300: string;
-//         400: string;
-//        };
